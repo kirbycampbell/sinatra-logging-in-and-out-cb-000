18ade4978bcac42918e7025ee27ca64b7512f23f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-01 02:21:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727222344) do
+ActiveRecord::Schema.define(version: 20180801014302) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.float "balance"
+  end
 
 end
